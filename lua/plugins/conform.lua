8003a5b2152e21f3588269354014c6268755a980
Kyle Jacobson <jacokyle01@gmail.com>
2025-10-09 21:55:29 -0500

    more formatting fixes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,10 +1,17 @@
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        typescript = { "prettierd" },
-        markdown = { "prettierd" },
-        ["markdown.mdx"] = { "prettierd" },
-      },
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      typescript = { "prettierd" },  -- runs prettierd for TS
+      typescriptreact = { "prettierd" }, -- also for TSX
+      javascript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      markdown = { "prettierd" },
+      ["markdown.mdx"] = { "prettierd" },
+    },
+    format_on_save = {
+      lsp_fallback = true,  -- fallback to LSP if no formatter found
+      timeout_ms = 500,     -- adjust if formatting is slow
     },
   },
+}
