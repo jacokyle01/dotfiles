c5153a8bd7588737fa62380c2863108f389bedb2
Kyle Jacobson <jacokyle01@gmail.com>
2025-10-09 21:41:09 -0500

    formatting plugin

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,10 @@
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettierd" },
+        markdown = { "prettierd" },
+        ["markdown.mdx"] = { "prettierd" },
+      },
+    },
+  },
